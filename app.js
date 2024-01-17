22ed4962a80b74f6038069fcecdabb16f5209796
Ziad Ali <ziaddalii11@gmail.com>
2024-01-18 01:17:22 +0200

    creating http server

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-const {log} = require("./logger")
+// Core Module
+const http = require("http");
+const books = [
+    {
+        id:1,
+        name:"book 1"
+    },
+    {
+        id:2,
+        name:"book 2"
+    },
+]
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("<h1>Welcome to NODE JS</h1>");
+    res.end();
+  }
+  if (req.url === "/api/books") {
+    res.write(JSON.stringify(books));
+    res.end();
+  }
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+const { log } = require("./logger");
 console.log("welcome to node js");
 
-const http = require("http");
-
-log()
+log();
