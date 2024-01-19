020d39eaae7084b16b8b9e8bb742a81a9a2970a8
Ziad Ali <ziaddalii11@gmail.com>
2024-01-19 14:58:18 +0200

    Created server using express js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Core Module
-const http = require("http");
+// const http = require("http");
 const books = [
     {
         id:1,
@@ -10,21 +10,41 @@ const books = [
         name:"book 2"
     },
 ]
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("<h1>Welcome to NODE JS</h1>");
-    res.end();
-  }
-  if (req.url === "/api/books") {
-    res.write(JSON.stringify(books));
-    res.end();
-  }
-});
+// CREATING SERVER USING NODE JS
 
-const PORT = 5000;
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write("<h1>Welcome to NODE JS</h1>");
+//     res.end();
+//   }
+//   if (req.url === "/api/books") {
+//     res.write(JSON.stringify(books));
+//     res.end();
+//   }
+// });
 
-const { log } = require("./logger");
-console.log("welcome to node js");
+// const PORT = 5000;
+// server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
-log();
+// const { log } = require("./logger");
+// console.log("welcome to node js");
+
+// log();
+
+// CREATING SERVER USING EXPRESS JS
+const express = require("express");
+
+// Init App
+const app = express();
+
+// HTTP METHODS
+app.get("/", (req, res) => {
+  res.send("Hello Welcome to express js ")
+})
+
+app.get("/api/books", (req, res) => {
+  res.json(books)
+})
+// Running the server
+const PORT = 5000
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
