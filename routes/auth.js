7a3539450e44ee9e59c72457b793e56f59351ad9
Ziad Ali <ziaddalii11@gmail.com>
2024-02-26 00:11:50 +0200

    Made Login Route

--- routes/auth.js
+++ routes/auth.js
@@ -36,3 +36,32 @@ module.exports = router;
     const {password, ...other} = result._doc
     res.status(201).json({...other, token})
   }))
+
+  {
+    /**
+     * @desc   Login User
+     * @route  /api/auth/login
+     * @method POST
+     * @access public
+     */
+  }
+
+  router.post("/login", asyncHandler(async (req, res) => {
+    const {error} = validateLoginUser(req.body)
+    if(error){
+        return res.status(400).json({message:error.details[0].message})
+    }
+    let user = await User.findOne({email: req.body.email});
+    if(!user){
+        res.status(400).json({message:"invalid email or password"});
+    }
+
+    const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+    if(!isPasswordMatch){
+        res.status(400).json({message:"invalid email or password"});
+    }
+
+    const token = null;
+    const {password, ...other} = user._doc
+    res.status(200).json({...other, token})
+  }))
